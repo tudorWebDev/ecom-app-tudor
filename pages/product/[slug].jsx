7fa413e04a116496c1f8ae44d86723af39812f7a
tudorWebDev <tudorbalexe@gmail.com>
2022-09-10 03:46:38 +0200

    first commit

--- pages/product/[slug].jsx
+++ pages/product/[slug].jsx
@@ -89,8 +89,8 @@ const ProductDetails = ({ product, products }) => {
         <h2>You may also like</h2>
         <div className="marquee">
           <div className="maylike-products-container track">
-            {products.map((item) => (
-              <Product key={item._id} product={item} />
+            {products.map( (item) => (
+               <Product key={item._id} product={item} />
             ))}
           </div>
         </div>
